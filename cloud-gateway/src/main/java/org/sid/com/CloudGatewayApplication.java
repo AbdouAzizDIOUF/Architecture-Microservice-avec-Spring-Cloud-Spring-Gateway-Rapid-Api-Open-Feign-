f63910a305d27c7bf721e609a814c8d9c75efe7e
AbdouAzizDIOUF <abdouazizdiouf75@gmail.com>
2020-04-20 00:08:18 +0200

    mise en place du service spring gateway et routage static via la main principale

--- cloud-gateway/src/main/java/org/sid/com/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/org/sid/com/CloudGatewayApplication.java
@@ -2,9 +2,20 @@ package org.sid.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CloudGatewayApplication {
+
+    @Bean
+    RouteLocator routes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r->r.path("/costumers/**").uri("http://localhost:9005").id("r1"))
+                .route(r->r.path("/products/**").uri("http://localhost:9006").id("r2"))
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CloudGatewayApplication.class, args);
