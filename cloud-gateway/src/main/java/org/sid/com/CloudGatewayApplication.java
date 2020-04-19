f3332cae33bb84f2f242de564a5b9ef9293d8e84
AbdouAzizDIOUF <abdouazizdiouf75@gmail.com>
2020-04-20 01:48:56 +0200

    mise en place du service spring gateway et routage dynamic

--- cloud-gateway/src/main/java/org/sid/com/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/org/sid/com/CloudGatewayApplication.java
@@ -2,6 +2,10 @@ package org.sid.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,12 +13,17 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CloudGatewayApplication {
 
-    @Bean
+    //@Bean
     RouteLocator routes(RouteLocatorBuilder builder){
         return builder.routes()
-                .route(r->r.path("/costumers/**").uri("http://localhost:9005").id("r1"))
-                .route(r->r.path("/products/**").uri("http://localhost:9006").id("r2"))
+                .route(r->r.path("/costumers/**").uri("lb://COSTUMER-SERVICE").id("r1"))
+                .route(r->r.path("/products/**").uri("lb://INVENTOR-SERVICE").id("r2"))
                 .build();
+    }
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
     }
 
     public static void main(String[] args) {
